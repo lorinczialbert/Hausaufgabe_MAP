3fb0b56e3d2f457a331c55f7437da958e67be98c
Albert Lorinczi <88albertlorinczi@gmail.com>
2025-10-18 17:16:57 +0300

    Punkt2 bzw. das testen

--- uebung_4/src/aufgabe4.java
+++ uebung_4/src/aufgabe4.java
@@ -16,6 +16,30 @@ public class aufgabe4 {
         }
         return billigsterPreis;
     }
+    //2. Teuersten Gegenstand finden
+    // Hilfsmethode, um den Maximalpreis in EINER Liste zu finden
+    private int findeMaximalenPreis(int[] preise) {
+        if (preise.length == 0) {
+            return -1;
+        }
+        int teuersterPreis = preise[0];
+        for (int preis : preise) {
+            if (preis > teuersterPreis) {
+                teuersterPreis = preis;
+            }
+        }
+        return teuersterPreis;
+    }
+
+    public int findeTeuerstenGegenstand(int[] tastaturen, int[] usbLaufwerke) {
+        int maxTastatur = findeMaximalenPreis(tastaturen);
+        int maxUsb = findeMaximalenPreis(usbLaufwerke);
+
+        // gibt den grsseren der beiden Maximalwerte zuruck
+        return Math.max(maxTastatur, maxUsb);
+    }
+
+
 
 
 
@@ -27,7 +51,13 @@ public class aufgabe4 {
         int[] tastaturPreise1 = {40, 35, 70, 15, 45};
         System.out.println("1. Billigste Tastatur: " + aufgabe.findeBilligsteTastatur(tastaturPreise1));
 
-        //
+        //2. Teuersten Gegenstand finden
+        int[] tastaturPreise2 = {15, 20, 10, 35};
+        int[] usbPreise2 = {20, 15, 40, 15};
+        System.out.println("2. Teuerster Gegenstand: " + aufgabe.findeTeuerstenGegenstand(tastaturPreise2, usbPreise2));
+
+        //3.
+
 
     }
 }

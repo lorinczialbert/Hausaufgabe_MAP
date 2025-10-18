834f4e2cd23acd0c82e042107f83f6ae4e6d5ed8
Albert Lorinczi <88albertlorinczi@gmail.com>
2025-10-18 16:46:53 +0300

    Aufgabe 3 summe und testen

--- uebung_3/src/aufgabe3.java
+++ uebung_3/src/aufgabe3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class aufgabe3 {
 
@@ -19,7 +20,20 @@ public class aufgabe3 {
         // Wenn nach der letzten Stelle noch ein Ubertrag ubrig ist, hinzufugen
         if (uebertrag > 0) {
             ergebnisListe.add(0, uebertrag);
-    }
+        }
         return ergebnisListe.stream().mapToInt(i -> i).toArray();
-}
 
+    }
+
+    public static void main(String[] args) {
+        aufgabe3 aufgabe = new aufgabe3();
+
+        //1. Summe
+        int[] s1= {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] s2= {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] summe = aufgabe.summe(s1, s2);
+        System.out.println("Die Summe ist: " + Arrays.toString(summe));
+
+        //
+    }
+}
